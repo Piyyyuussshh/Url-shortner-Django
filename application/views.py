ae9005d222e194285a457e6c6ad6e04f2cd72c0e
Piyush <piyush.mehra13@gmail.com>
2022-05-19 12:25:52 +0530

    Modified database and created urls.

--- application/views.py
+++ application/views.py
@@ -1,7 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+import uuid
+from .models import Url
 # Create your views here.
 
 
 def home(req):
     return render(req, 'main.html')
+
+
+def create(req):
+    if req.method == "POST":
+        link = req.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
